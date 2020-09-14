65cd3812099cc0913bb4e041d8d98ebba6496a16
xiaoyao <1522079158@qq.com>
2020-09-14 20:05:01 +0800

    `test`

--- src/test/scala/ScalaLearn.scala
+++ src/test/scala/ScalaLearn.scala
@@ -1,3 +1,35 @@
+import scala.io.Source
+
 class ScalaLearn {
 
+}
+
+
+object ScalaLearn {
+
+  def main(args: Array[String]): Unit = {
+    processFile("./src/test/scala/ScalaLearn.scala",45)
+  }
+
+  def processFile(fileName: String, width: Int) = {
+
+    def processLines(line: String) = {
+
+      if(line.length > width)
+        print(fileName + ":" + line)
+
+    }
+
+    val source = Source.fromFile(fileName)
+    for (line <- source.getLines()) {
+      processLines(line)
+    }
+  }
+//
+//  private def processLines(fileName: String, line: String, width: Int) = {
+//    if (line.length > width) {
+//      println(fileName + ":" + line.trim)
+//    }
+
+  }
 }
