a3866b5a2463ab5f39b71ca64203266a4f82c630
xiaoyao <1522079158@qq.com>
2020-12-24 17:53:53 +0800

    新增 TransformTest

---
+++ flinkModel/src/main/scala/com/choyao/apitest/TransformTest.scala
@@ -0,0 +1,12 @@
+package com.choyao.apitest
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object TransformTest {
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  }
+}
