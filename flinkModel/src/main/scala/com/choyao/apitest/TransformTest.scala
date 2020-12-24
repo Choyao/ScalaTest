8e39c41dd33aa20afe0d09dfac71306e6194fb9b
xiaoyao <1522079158@qq.com>
2020-12-25 00:09:13 +0800

    updata

--- flinkModel/src/main/scala/com/choyao/apitest/TransformTest.scala
+++ flinkModel/src/main/scala/com/choyao/apitest/TransformTest.scala
@@ -1,12 +1,27 @@
 package com.choyao.apitest
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.commons.io.IOUtils
+import org.apache.flink.streaming.api.scala._
 
 object TransformTest {
 
   def main(args: Array[String]): Unit = {
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    val lineStream = env.readTextFile("C:\\Project\\ScalaTest\\flinkModel\\src\\main\\resources\\sensor.txt")
+
+
+    val sensorStream = lineStream.map(data=> {
+      val list = data.split(",")
+      Sensor(list(0),list(1).toLong,list(2).toDouble)
+
+    })
+    sensorStream.keyBy("id")
+      .minBy("temperature")
+      .print()
+
+    env.execute("Transform Test")
 
   }
 }
