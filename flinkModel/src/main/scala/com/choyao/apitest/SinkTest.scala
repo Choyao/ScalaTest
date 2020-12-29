3c4a84c8d96177962189dccdefca5bb83718d2f4
xiaoyao <1522079158@qq.com>
2020-12-29 18:01:51 +0800

    新增 SinkTest

---
+++ flinkModel/src/main/scala/com/choyao/apitest/SinkTest.scala
@@ -0,0 +1,20 @@
+package com.choyao.apitest
+
+import org.apache.flink.streaming.api.scala._
+
+object SinkTest {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val inputStream = env.readTextFile(SinkTest.getClass.getClassLoader.getResource("") + "../../src/main/resources/sensor.txt")
+
+
+    val outputStream = inputStream.map(data => {
+      val l = data.split(",")
+      Sensor(l(0),l(1).toLong,l(2).toDouble)
+    })
+    outputStream.writeAsCsv(SinkTest.getClass.getClassLoader.getResource("") + "../../src/main/resources/sensor.csv")
+
+
+    env.execute("sink test")
+  }
+}
