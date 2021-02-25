001f402c5964a2895a22566a13788dd33f60036e
xiaoyao <1522079158@qq.com>
2021-02-25 14:47:44 +0800

    scala Blink 流处理

--- flinkModel/src/main/scala/com/choyao/apitest/tabletest/TableTest.scala
+++ flinkModel/src/main/scala/com/choyao/apitest/tabletest/TableTest.scala
@@ -3,7 +3,7 @@ package com.choyao.apitest.tabletest
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.table.api.{DataTypes, EnvironmentSettings}
 import org.apache.flink.table.api.scala._
-import org.apache.flink.table.descriptors.{FileSystem, OldCsv, Schema}
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Kafka, OldCsv, Schema}
 
 
 object TableTest {
@@ -17,14 +17,36 @@ object TableTest {
     val tableEnv = StreamTableEnvironment.create(env, set)
 
 
-    tableEnv.connect(new FileSystem().path(TableTest.getClass.getClassLoader.getResource("") + "../../src/main/resources/sensor.txt"))
-      .withFormat(new OldCsv)
+    /*
+    flink  读取 本地文件
+     */
+    /*    tableEnv.connect(new FileSystem().path(TableTest.getClass.getClassLoader.getResource("") + "../../src/main/resources/sensor.txt"))
+          .withFormat(new OldCsv)
+          .withSchema(new Schema().field("id", DataTypes.STRING())
+            .field("tmp", DataTypes.BIGINT())
+            .field("temp", DataTypes.DOUBLE()))
+          .createTemporaryTable("sensor")
+
+        val table = tableEnv.sqlQuery("select * from sensor")
+        tableEnv.toRetractStream[(String, Long, Double)](table).print()
+        */
+
+    /*
+    flink  读取kafka
+     */
+    tableEnv.connect(new Kafka()
+      .version("0.10")
+      .property("zookeeper.connect", "192.168.10.151:2181")
+      .property("bootstrap.servers", "192.168.10.151:9092")
+      .topic("sensor")
+    ).withFormat(new Csv)
       .withSchema(new Schema().field("id", DataTypes.STRING())
         .field("tmp", DataTypes.BIGINT())
         .field("temp", DataTypes.DOUBLE()))
       .createTemporaryTable("sensor")
 
-    val table = tableEnv.sqlQuery("select * from sensor")
+
+    val table = tableEnv.from("sensor")
     tableEnv.toRetractStream[(String, Long, Double)](table).print()
 
     tableEnv.execute("table test")
